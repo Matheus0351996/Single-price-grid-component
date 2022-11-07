9d54f559a0cd924604c09155935cc3722e0a1266
matheus <mnatalsantos@hotmail.com>
2022-11-07 16:04:58 -0300

    conclusão

--- single-price/src/components/Card/card.js
+++ single-price/src/components/Card/card.js
@@ -1,22 +1,29 @@
-import { BluePart, Paragraph, SubTitle, Title } from "./styled"
+import { BluePart, Button, Container, DarkBluePart, Flex, LightBluePart, Paragraph, Price, SubTitle, Title } from "./styled"
 import { bluePart } from "./styled"
 
 
 const Card = () => {
     return(
-        <div>
+        <Container>
             <Title>Join our community</Title>
                 <SubTitle>30-day, hassle-free money back guarantee</SubTitle>
-                    <Paragraph>Gain acess to our full library of tutorials along with expert code reviews,</Paragraph>
-                    <Paragraph>Perfect for any developers who are serious about honing their skills</Paragraph>
+                <Paragraph>
+                    <p>Gain acess to our full library of tutorials along with expert code reviews,</p>
+                    <p>Perfect for any developers who are serious about honing their skills</p>
+                    </Paragraph>
                     <BluePart>
-                <div>
+                <DarkBluePart>
                     <h3>Monthly Subscription</h3>
-                    <p>$29 per month</p>
-                    <p>Full access for less than $1 a day</p>
-                    <button>Sign Up</button>
-                </div>
-                <div> 
+                    <Price>
+                    <p className="price">$29</p>
+                    <p className="per-month">per month</p>
+                    </Price>
+                    <p className="acess">Full access for less than $1 a day</p>
+                    <Button>
+                    <a>Sign Up</a>
+                    </Button>
+                </DarkBluePart>
+                <LightBluePart> 
                 <h3>Why Us</h3>
                     <p>Tutorials by industry experts</p>
                     <p>Peer & expert code review</p>
@@ -25,9 +32,9 @@ const Card = () => {
                     <p>Community forum</p>
                     <p>Flashcard decks</p>
                     <p>New videos every week</p>
-                </div>
+                </LightBluePart>
         </BluePart>
-        </div>
+        </Container>
     )
 }
 
